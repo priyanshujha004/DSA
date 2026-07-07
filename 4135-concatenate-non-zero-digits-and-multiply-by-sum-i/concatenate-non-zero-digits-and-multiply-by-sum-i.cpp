d2fda68cb5af3823e0a2930cf53d8cb55b41a51f
Priyanshu Jha <priyanshujha971@gmail.com>
2026-07-08 01:34:20 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (52.06%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -3,7 +3,7 @@ public:
     long long sumAndMultiply(int n) {
         if(n==0) return 0;
         long long sum = 0; long long num = 0; long long place = 1;
-        long long temp = std::abs(static_cast<long long>(n));
+        long long temp = n; if(temp<0) temp=-temp;
         while(temp!=0){
             int dig = temp%10;
             if(dig!=0){
