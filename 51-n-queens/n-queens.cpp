937792b9ad24af528635d07237fbc0456d9f7600
Priyanshu Jha <priyanshujha971@gmail.com>
2026-08-08 00:31:59 +0530

    Time: 3 ms (61.40%) | Memory: 10 MB (94.43%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     bool isSafe(vector<string> &board, int row, int col, int n){
-        for(int idx = 0; idx < n; idx++){
+        for(int idx=0; idx<n; idx++){ // Checking No Queens on Same Row
             if(board[row][idx]=='Q') return false;
         }
-        for(int jdx = 0; jdx < n; jdx++){
+        for(int jdx=0; jdx<n; jdx++){ // Checking No Queens on Same Column
             if(board[jdx][col]=='Q') return false;
         }
-        for(int idx = row, jdx = col; idx>=0 && jdx>=0; idx--, jdx--){
-            if(board[idx][jdx]=='Q') return false;
+        for(int pdx=row,qdx=col; pdx>=0 && qdx>=0; pdx--,qdx--){ // Checking No Queens on Left Diagonal
+            if(board[pdx][qdx]=='Q') return false;
         }
-        for(int idx = row, jdx = col; idx>=0 && jdx<n; idx--, jdx++){
-            if(board[idx][jdx]=='Q') return false;
+        for(int rdx=row,sdx=col; rdx>=0 && sdx<n; rdx--,sdx++){ // Checking No Queens on Right Diagonal
+            if(board[rdx][sdx]=='Q') return false;
         }
         return true;
     }
-    void nQueens(vector<string> &board, vector<vector<string>> &ans, int row, int n){
-        if(n==row){
-            ans.push_back({board});
+    void nQueens(vector<string> &board, vector<vector<string>> &answer, int row, int n){
+        if(row==n){
+            answer.push_back({board});
             return;
         }
-        for(int col=0; col<n; col++){
+        for(int col = 0; col < n; col++){
             if(isSafe(board,row,col,n)){
                 board[row][col]='Q';
-                nQueens(board,ans,row+1,n);
+                nQueens(board,answer,row+1,n);
                 board[row][col]='.';
             }
         }
+
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.')); 
-        vector<vector<string>> ans;
-        nQueens(board,ans,0,n);
-        return ans;
+        vector<string> board(n,string(n,'.'));
+        vector<vector<string>> answer;
+        nQueens(board,answer,0,n);
+        return answer;
     }
 };
